cef7759fd5c0e18666edb8932273c49d59639e43
Michael Pearson <mfpears@gmail.com>
2023-02-11 01:44:46 -0700

    Update

---
+++ 911.c5aef2f916e5dd19.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[911],{4911:(i,e,u)=>{function l(o){const s=t=>o.frameworksMono=o.featuresMono=t;setTimeout(()=>s(!1),1e3),window.addEventListener("scroll",()=>{o.monoTimeout&&clearTimeout(o.monoTimeout),s(!0),o.monoTimeout=setTimeout(()=>s(!1),300)})}u.r(e),u.d(e,{setUpScrollColors:()=>l})}}]);
